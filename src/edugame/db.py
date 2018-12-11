9df04ca4549389f32c5a17a7160a1210f7356680
Hang Liang <31755047+w1476423@users.noreply.github.com>
2018-12-10 18:37:00 -0800

    Update db.py
    test open & close db - successful

--- src/edugame/db.py
+++ src/edugame/db.py
@@ -1,59 +1,87 @@
 import datetime
 import sqlite3
+# import MySQLdb
+import mysql.connector as mysql
+from mysql.connector import errorcode
 
-from edugame.api import GameSession
+db_master = 'mindgame_db'
+db_host='134.173.236.104'
+db_user='mindgame'
+db_password='password'
+db_table=''
 
-"""
-Data access component for games
-"""
+def open_test_close():
+    try:
+        cnx=mysql.connect(user=db_user,password=db_password, host=db_host, database=db_master)
+        print("connection opened")
+    except mysql.Error as err:
 
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
 
-class DataAccess:
-
-    CREATE_TABLE_STATEMENT = \
-        '''create table if not exists game_history 
-                                     (ID                INTEGER  PRIMARY KEY, 
-                                      USER_ID           INTEGER,   
-                                      DATE_PLAYED       DATE     NOT NULL, 
-                                      TOTAL_TIME_PLAYED REAL     NOT NULL, 
-                                      SCORE             REAL     NOT NULL,
-                                      GAME STATISTICS   REAL     NOT NULL)'''
-
-    INSERT_STATEMENT = \
-        '''insert into game_history (USER_ID, DATE_PLAYED, TOTAL_TIME_PLAYED, SCORE) values (?,?,?,?)'''
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.connection = sqlite3.connect(database="statistics.db")
-        self.setup()
-
-    def setup(self):
-        # create tables
-        c = self.connection.cursor()
-        c.execute(self.CREATE_TABLE_STATEMENT)
-
-        pass
-
-    def save_game_session(self, game_session:GameSession):
-        # TODO: validate
-        c = self.connection.cursor()
-
-        c.execute(self.INSERT_STATEMENT, (game_session.user_id,
-                                          datetime.date.today(),
-                                          game_session.time_played,
-                                          game_session.score))
-
-        self.connection.commit()
+    else:
+        cnx.close()
+        print ('Connection Closed')
+open_test_close()
 
 
-if __name__ == '__main__':
-    data_access = DataAccess()
-    print(data_access)
+# from edugame.api import GameSession
 
-    data_access.save_game_session(GameSession(1, 200, 20))
+# """
+# Data access component for games
+# """
 
-    connection = sqlite3.connect(database="../statistics.db")
-    c = connection.cursor()
-    c.execute('''SELECT score FROM game_history''')
-    row = c.fetchone()
-    print(row[0])
+
+# class DataAccess:
+
+#     CREATE_TABLE_STATEMENT = \
+#         '''create table if not exists game_history 
+#                                      (ID                INTEGER  PRIMARY KEY, 
+#                                       USER_ID           INTEGER,   
+#                                       DATE_PLAYED       DATE     NOT NULL, 
+#                                       TOTAL_TIME_PLAYED REAL     NOT NULL, 
+#                                       SCORE             REAL     NOT NULL,
+#                                       GAME STATISTICS   REAL     NOT NULL)'''
+
+#     INSERT_STATEMENT = \
+#         '''insert into game_history (USER_ID, DATE_PLAYED, TOTAL_TIME_PLAYED, SCORE) values (?,?,?,?)'''
+
+#     def __init__(self) -> None:
+#         super().__init__()
+#         self.connection = sqlite3.connect(database="statistics.db")
+#         self.setup()
+
+#     def setup(self):
+#         # create tables
+#         c = self.connection.cursor()
+#         c.execute(self.CREATE_TABLE_STATEMENT)
+
+#         pass
+
+#     def save_game_session(self, game_session:GameSession):
+#         # TODO: validate
+#         c = self.connection.cursor()
+
+#         c.execute(self.INSERT_STATEMENT, (game_session.user_id,
+#                                           datetime.date.today(),
+#                                           game_session.time_played,
+#                                           game_session.score))
+
+#         self.connection.commit()
+
+
+# if __name__ == '__main__':
+#     data_access = DataAccess()
+#     print(data_access)
+
+#     data_access.save_game_session(GameSession(1, 200, 20))
+
+#     connection = sqlite3.connect(database="../statistics.db")
+#     c = connection.cursor()
+#     c.execute('''SELECT score FROM game_history''')
+#     row = c.fetchone()
+#     print(row[0])
